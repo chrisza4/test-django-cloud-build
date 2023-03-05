45238589c18e81cbb8e5e565b4a350cb3abd398e
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2023-03-05 16:43:31 +0700

    try run test on docker

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-curl -sSL https://install.python-poetry.org | python3 -
-export PATH="/builder/home/.local/bin:$PATH"
-poetry install
+# curl -sSL https://install.python-poetry.org | python3 -
+# export PATH="/builder/home/.local/bin:$PATH"
+# poetry install
 cd basic_django
 poetry run python manage.py test
